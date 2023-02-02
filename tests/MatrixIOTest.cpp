2575b84e90a546a34514e0466bf266e56f829f4c
Balázs Kis <balazs_kis35@yahoo.com>
2023-02-02 17:02:52 +0100

    Add changes for initial test and formatting step.

---
+++ tests/MatrixIOTest.cpp
@@ -0,0 +1,33 @@
+#define BOOST_TEST_MODULE SideMadeTests
+#define BOOST_TEST_DYN_LINK
+#include <Eigen/Dense>
+#include <boost/test/unit_test.hpp>
+#include "matrixIO.hpp"
+
+using namespace Eigen;
+
+struct MatrixIOFixture {
+  MatrixIOFixture()
+  {
+    mat = MatrixXd(3, 3);
+    mat << 1, 0, 0,
+        0, 1, 0,
+        0, 0, 1;
+  }
+
+  MatrixXd mat;
+};
+
+BOOST_FIXTURE_TEST_SUITE(MatrixIOTests, MatrixIOFixture)
+
+BOOST_AUTO_TEST_CASE(MyOnlyTest)
+{
+  saveData("../data/text.csv", mat);
+
+  MatrixXd output;
+  output = openData("../data/text.csv", 3);
+
+  BOOST_TEST(mat == output);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
