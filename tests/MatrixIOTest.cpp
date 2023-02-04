0708b9537b679bda11fc0898d662835a658f9538
Balázs Kis <balazs_kis35@yahoo.com>
2023-02-04 20:15:20 +0100

    Delete not needed imports.

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,6 +1,4 @@
-#define BOOST_TEST_MODULE SideMadeTests
 #define BOOST_TEST_DYN_LINK
-#include <Eigen/Dense>
 #include <boost/test/unit_test.hpp>
 #include "matrixIO.hpp"
 
@@ -25,7 +23,7 @@ BOOST_AUTO_TEST_CASE(MyOnlyTest)
   matrixIO::saveData("../data/text.csv", mat);
 
   MatrixXd output = matrixIO::openData("../data/text.csv", 3);
-  BOOST_TEST(mat(0, 0) == output(0, 0));
+  BOOST_TEST(mat == output);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
